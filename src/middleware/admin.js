7d222b52d8342b22a6865011f40d524463fb4beb
aaditya nair <aadityanairaaq@gmail.com>
2024-09-03 19:31:32 +0300

    reduced-cognitive-complexity-to-15

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -1,8 +1,6 @@
 'use strict';
 
-
 const nconf = require('nconf');
-
 const user = require('../user');
 const meta = require('../meta');
 const plugins = require('../plugins');
@@ -27,45 +25,61 @@ middleware.buildHeader = helpers.try(async (req, res, next) => {
 });
 
 middleware.checkPrivileges = helpers.try(async (req, res, next) => {
-	// Kick out guests, obviously
-	if (req.uid <= 0) {
+	if (isGuest(req, res)) {
+		return;
+	}
+
+	const path = req.path.replace(/^(\/api)?(\/v3)?\/admin\/?/g, '');
+	if (!(await hasPrivilegeForPath(req, path))) {
 		return controllers.helpers.notAllowed(req, res);
 	}
 
-	// Otherwise, check for privilege based on page (if not in mapping, deny access)
-	const path = req.path.replace(/^(\/api)?(\/v3)?\/admin\/?/g, '');
+	if (!await userHasPassword(req)) {
+		return next();
+	}
+
+	if (shouldRelogin(req)) {
+		await handleRelogin(req, res);
+	} else {
+		extendLogoutTimer(req);
+		return next();
+	}
+});
+
+// Function to check if the user is a guest
+function isGuest(req, res) {
+	if (req.uid <= 0) {
+		controllers.helpers.notAllowed(req, res);
+		return true;
+	}
+	return false;
+}
+
+// Function to check if the user has the necessary privilege for the requested path
+async function hasPrivilegeForPath(req, path) {
 	if (path) {
 		const privilege = privileges.admin.resolve(path);
-		if (!await privileges.admin.can(privilege, req.uid)) {
-			return controllers.helpers.notAllowed(req, res);
-		}
+		return await privileges.admin.can(privilege, req.uid);
 	} else {
-		// If accessing /admin, check for any valid admin privs
 		const privilegeSet = await privileges.admin.get(req.uid);
-		if (!Object.values(privilegeSet).some(Boolean)) {
-			return controllers.helpers.notAllowed(req, res);
-		}
+		return Object.values(privilegeSet).some(Boolean);
 	}
+}
 
-	// If user does not have password
-	const hasPassword = await user.hasPassword(req.uid);
-	if (!hasPassword) {
-		return next();
-	}
+// Function to check if the user has a password
+async function userHasPassword(req) {
+	return await user.hasPassword(req.uid);
+}
 
-	// Reject if they need to re-login (due to ACP timeout), otherwise extend logout timer
+// Function to determine if the user needs to re-login
+function shouldRelogin(req) {
 	const loginTime = req.session.meta ? req.session.meta.datetime : 0;
 	const adminReloginDuration = meta.config.adminReloginDuration * 60000;
-	const disabled = meta.config.adminReloginDuration === 0;
-	if (disabled || (loginTime && parseInt(loginTime, 10) > Date.now() - adminReloginDuration)) {
-		const timeLeft = parseInt(loginTime, 10) - (Date.now() - adminReloginDuration);
-		if (req.session.meta && timeLeft < Math.min(60000, adminReloginDuration)) {
-			req.session.meta.datetime += Math.min(60000, adminReloginDuration);
-		}
+	return !(meta.config.adminReloginDuration === 0 || (loginTime && parseInt(loginTime, 10) > Date.now() - adminReloginDuration));
+}
 
-		return next();
-	}
-
+// Function to handle user re-login
+async function handleRelogin(req, res) {
 	let returnTo = req.path;
 	if (nconf.get('relative_path')) {
 		returnTo = req.path.replace(new RegExp(`^${nconf.get('relative_path')}`), '');
@@ -76,13 +90,21 @@ middleware.checkPrivileges = helpers.try(async (req, res, next) => {
 	req.session.forceLogin = 1;
 
 	await plugins.hooks.fire('response:auth.relogin', { req, res });
-	if (res.headersSent) {
-		return;
+	if (!res.headersSent) {
+		if (res.locals.isAPI) {
+			controllers.helpers.formatApiResponse(401, res);
+		} else {
+			res.redirect(`${nconf.get('relative_path')}/login?local=1`);
+		}
 	}
+}
 
-	if (res.locals.isAPI) {
-		controllers.helpers.formatApiResponse(401, res);
-	} else {
-		res.redirect(`${nconf.get('relative_path')}/login?local=1`);
+// Function to extend the user's logout timer
+function extendLogoutTimer(req) {
+	const loginTime = req.session.meta ? req.session.meta.datetime : 0;
+	const adminReloginDuration = meta.config.adminReloginDuration * 60000;
+	const timeLeft = parseInt(loginTime, 10) - (Date.now() - adminReloginDuration);
+	if (req.session.meta && timeLeft < Math.min(60000, adminReloginDuration)) {
+		req.session.meta.datetime += Math.min(60000, adminReloginDuration);
 	}
-});
+}
